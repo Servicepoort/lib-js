6635c1ec4f6e34383984ecff9a04eed319fbdccf
ben-compaan <70568579+ben-compaan@users.noreply.github.com>
2020-12-18 14:06:01 +0100

    loadEnv: only show message when overriden vars apply

--- debug/load-env.js
+++ debug/load-env.js
@@ -4,10 +4,9 @@ module.exports = (path) => {
         for (let [key, value] of Object.entries(env)) {
             process.env[key] = value.toString();
         }
+        console.log('Using ENV variables from file ' + path)
     } catch (error) {
-        if (error.code === 'MODULE_NOT_FOUND') {
-            console.log('No custom env variables found, using server defaults. This is OK on staging / production.\n\n');
-        } else {
+        if (error.code !== 'MODULE_NOT_FOUND') {
             console.log('Error reading or processing env file:', { error }, '\n\n');
         }
     }
